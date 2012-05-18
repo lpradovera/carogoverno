093f7df3a40d4a2319fc5233a087367b11b855e5
Luca Pradovera <luca.pradovera@gmail.com>
2012-05-18 23:24:57 +0200

    Google Analytics e checkbox privacy su registrazione.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :confirmed_at, :is_admin
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :confirmed_at, :is_admin, :terms_of_service, :privacy_terms
   has_many :posts
   has_many :votes
   validates_length_of :name, :maximum => 15, :message=>"Lunghezza massima %d caratteri"
   validates_uniqueness_of :name
+  validates_acceptance_of :terms_of_service, :on => :create
+  validates_acceptance_of :privacy_terms, :on => :create
   # attr_accessible :title, :body
   
   def gravatar
