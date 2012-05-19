9fe3fd96ffcece8f7a03a64b6d4c976e86eb7bac
Luca Pradovera <luca.pradovera@gmail.com>
2012-05-19 17:34:35 +0200

    Mailer configurato

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Carogoverno::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'carogoverno.it' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
